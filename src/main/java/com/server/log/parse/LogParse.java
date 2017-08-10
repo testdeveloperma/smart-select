12142a6dfa34a27f8b46ec828a8ef6ba31c13969
testdeveloperma <cj2013by@163.com>
2017-08-10 16:00:37 +0800

    first commit

---
+++ src/main/java/com/server/log/parse/LogParse.java
@@ -0,0 +1,9 @@
+package com.server.log.parse;
+
+public class LogParse {
+
+	
+	
+	
+	
+}
---
+++ src/main/java/com/server/log/parse/ReqParam.java
@@ -0,0 +1,15 @@
+package com.server.log.parse;
+
+public class ReqParam {
+
+	private String url;
+	
+	private String req;
+	
+	private String resp;
+	
+	
+	
+	
+	
+}
