8e54af1eae729c00a1bdc9654994cc42219ca8b3
ELONG\chengjun.ma <chengjun.ma@elong.com>
2017-08-10 19:04:59 +0800

    add sql

--- src/main/java/com/server/log/parse/ReqParam.java
+++ src/main/java/com/server/log/parse/ReqParam.java
@@ -2,11 +2,60 @@ package com.server.log.parse;
 
 public class ReqParam {
 
+	private Integer id;
+	
 	private String url;
+	
+	private String header;
 	
 	private String req;
 	
 	private String resp;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getHeader() {
+		return header;
+	}
+
+	public void setHeader(String header) {
+		this.header = header;
+	}
+
+	public String getReq() {
+		return req;
+	}
+
+	public void setReq(String req) {
+		this.req = req;
+	}
+
+	public String getResp() {
+		return resp;
+	}
+
+	public void setResp(String resp) {
+		this.resp = resp;
+	}
+
+	@Override
+	public String toString() {
+		return "ReqParam [id=" + id + ", url=" + url + ", header=" + header + ", req=" + req + ", resp=" + resp + "]";
+	}
 	
 	
 	
